c1c882852c8b958538e947247c0c7a9f9d5c5c1c
sunshead <sunshead@gmail.com>
2016-06-11 22:31:51 -0400

    Building Views and Templates

--- django_inv/urls.py
+++ django_inv/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from inventory import views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'django_inv.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
+    url(r'^item/(?P<id>\d+)/', views.item_detail, name='item_detail'), #name group -- the digit characters will be called id
     url(r'^admin/', include(admin.site.urls)),
 ]
